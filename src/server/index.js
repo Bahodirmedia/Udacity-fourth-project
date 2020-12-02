54d981fce47d3b76c964a5a78e0525be2c4b917f
Bahodir Abdullayev <60607484+Bahodirmedia@users.noreply.github.com>
2020-12-03 00:48:24 +0500

    changes

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 const fetch = require('node-fetch')
@@ -27,7 +26,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('dist'))
 
-console.log(JSON.stringify(mockAPIResponse))
+// console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
